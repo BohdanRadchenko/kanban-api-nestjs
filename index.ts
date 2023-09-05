ce896750afe10347246ffe0e06a7a0d961d6afe8
Bohdan <radchenkobs@gmail.com>
2023-09-06 02:03:53 +0300

    status cors err

--- index.ts
+++ index.ts
@@ -1,18 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { ExpressAdapter } from '@nestjs/platform-express';
-import cors from 'cors';
+import * as cors from 'cors';
 import 'dotenv/config';
 import * as express from 'express';
 import * as functions from 'firebase-functions';
 import { AppModule } from './src/app.module';
 
-const corsOptions = {
-	origin: process.env.CLIENT
-};
-
 const expressServer = express();
 
-expressServer.use(cors(corsOptions));
+expressServer.use(cors({ origin: true }));
 
 const createFunction = async (expressInstance): Promise<void> => {
 	const app = await NestFactory.create(
