e1478a20f87218838d94fcdc8b7a0f03ad6f2bd7
Bohdan <radchenkobs@gmail.com>
2023-09-06 01:47:44 +0300

    add cors to express

--- index.ts
+++ index.ts
@@ -1,10 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { ExpressAdapter } from '@nestjs/platform-express';
+import cors from 'cors';
 import * as express from 'express';
 import * as functions from 'firebase-functions';
 import { AppModule } from './src/app.module';
 
+const corsOptions = {
+	origin: process.env.CLIENT
+};
+
 const expressServer = express();
+
+expressServer.use(cors(corsOptions));
+
 const createFunction = async (expressInstance): Promise<void> => {
 	const app = await NestFactory.create(
 		AppModule,
@@ -12,6 +20,7 @@ const createFunction = async (expressInstance): Promise<void> => {
 	);
 	await app.init();
 };
+
 export const api = functions.https.onRequest(async (request, response) => {
 	await createFunction(expressServer);
 	expressServer(request, response);
